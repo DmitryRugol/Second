d18534d799398b0e32d5753fdd4c3f11872ede12
DmitryRugol <dmitry.rugol@gmail.com>
2023-05-20 15:41:54 +0300

    First Commit

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MainController {
+
+    @GetMapping("/")
+    public @ResponseBody String hello() {
+        return "hello";
+    }
+}
