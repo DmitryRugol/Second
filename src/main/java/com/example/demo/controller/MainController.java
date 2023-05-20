7a11d763366aa7faba697932a179d789be49e6e8
DmitryRugol <dmitry.rugol@gmail.com>
2023-05-20 15:54:00 +0300

    First Commit 2

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class MainController {
 
+
     @GetMapping("/")
     public @ResponseBody String hello() {
         return "hello";
